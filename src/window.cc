94385689d30254a540694d557b2cda1af7d024f5
Jørgen Lien Sellæg <jorgen@guut.org>
2016-04-09 18:20:05 +0200

    feature: auto reload plugins on save and expand api

--- src/window.cc
+++ src/window.cc
@@ -391,6 +391,21 @@ void Window::set_menu_actions() {
             Notebook::get().get_view(c)->configure();
             Notebook::get().configure(c);
           }
+          if(view->file_path>Config::get().python.plugin_directory){
+            auto stem=view->file_path.stem().string();
+            auto module=PythonInterpreter::get().get_loaded_module(stem);
+            if(module){
+              auto module_new=pybind11::module(PyImport_ReloadModule(module.ptr()),false);
+              if(module_new)
+                Terminal::get().print("Python module "+stem + " has been reloaded \n");
+              else PythonError();
+            }else{
+              PythonError();
+              module=PythonInterpreter::get().import(stem);
+              if(module)
+                Terminal::get().print("Python module "+stem + " has been reloaded \n");
+            }
+          }
         }
         if(view->file_path.extension().string()==".py"){
           auto file_path=view->file_path;
